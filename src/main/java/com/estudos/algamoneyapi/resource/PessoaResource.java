ca2257b8ef499501667cbaecc98bfd073dec80a9
Janayna <fernandesmjanayna@gmail.com>
2020-04-20 09:24:14 -0300

    aula 4.1

--- src/main/java/com/estudos/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/estudos/algamoneyapi/resource/PessoaResource.java
@@ -1,28 +1,19 @@
 package com.estudos.algamoneyapi.resource;
 
 
-import java.net.URI;
-import java.util.List;
-import java.util.Optional;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-
 import com.estudos.algamoneyapi.event.RecursoCriadoEvent;
-import com.estudos.algamoneyapi.model.Categoria;
 import com.estudos.algamoneyapi.model.Pessoa;
 import com.estudos.algamoneyapi.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -51,6 +42,12 @@ public class PessoaResource {
     public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
         Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
         return pessoa.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{codigo}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long codigo) {
+        pessoaRepository.deleteById(codigo);
     }
 
 }
