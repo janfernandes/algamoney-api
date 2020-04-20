6a0d8486fe5084cc155a3cc714b7c1fb076ada5b
Janayna <fernandesmjanayna@gmail.com>
2020-04-20 10:59:03 -0300

    aula 4.4

--- src/main/java/com/estudos/algamoneyapi/service/PessoaService.java
+++ src/main/java/com/estudos/algamoneyapi/service/PessoaService.java
@@ -17,12 +17,25 @@ public class PessoaService {
     private PessoaRepository pessoaRepository;
 
     public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+        Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+
+        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+        return pessoaRepository.save(pessoaSalva);
+    }
+
+    public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+        Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+
+        pessoaSalva.setAtivo(ativo);
+        pessoaRepository.save(pessoaSalva);
+    }
+
+    private Pessoa buscarPessoaPeloCodigo(Long codigo) {
         Optional<Pessoa> pessoaSalva = pessoaRepository.findById(codigo);
         if(!pessoaSalva.isPresent()){
             throw new EmptyResultDataAccessException(1);
         }
-        BeanUtils.copyProperties(pessoa, pessoaSalva.get(), "codigo");
-        return pessoaRepository.save(pessoaSalva.get());
+        return pessoaSalva.get();
     }
 
 }
