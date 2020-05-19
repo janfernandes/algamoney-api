bf27d83000a93af70ad0d89f31eb5e174d04b3fc
Janayna Moura <janayna.fernandes@bradesco.com.br>
2020-05-19 12:04:49 -0300

    aula 6.10 cors

--- src/test/java/com/estudos/algamoneyapi/AlgamoneyApplicationTests.java
+++ src/test/java/com/estudos/algamoneyapi/AlgamoneyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AlgamoneyApplicationTests {
 
 	@Test
 	void contextLoads() {
