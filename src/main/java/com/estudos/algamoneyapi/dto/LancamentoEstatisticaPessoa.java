3e35ec1276ae2abc6d53db3189b795fd1531a09d
Janayna <fernandesmjanayna@gmail.com>
2020-08-02 21:41:58 -0300

    aula 22.8

---
+++ src/main/java/com/estudos/algamoneyapi/dto/LancamentoEstatisticaPessoa.java
@@ -0,0 +1,42 @@
+package com.estudos.algamoneyapi.dto;
+
+import com.estudos.algamoneyapi.model.Pessoa;
+import com.estudos.algamoneyapi.model.TipoLancamento;
+
+import java.math.BigDecimal;
+
+public class LancamentoEstatisticaPessoa {
+    private TipoLancamento tipoLancamento;
+    private Pessoa pessoa;
+    private BigDecimal total;
+
+    public LancamentoEstatisticaPessoa(TipoLancamento tipoLancamento, Pessoa pessoa, BigDecimal total) {
+        this.tipoLancamento = tipoLancamento;
+        this.pessoa = pessoa;
+        this.total = total;
+    }
+
+    public TipoLancamento getTipoLancamento() {
+        return tipoLancamento;
+    }
+
+    public void setTipoLancamento(TipoLancamento tipoLancamento) {
+        this.tipoLancamento = tipoLancamento;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+}
--- src/main/java/com/estudos/algamoneyapi/model/TipoLancamento.java
+++ src/main/java/com/estudos/algamoneyapi/model/TipoLancamento.java
@@ -1,6 +1,15 @@
 package com.estudos.algamoneyapi.model;
 
 public enum TipoLancamento {
-    RECEITA,
-    DESPESA
+    RECEITA("Receita"),
+    DESPESA("Despesa");
+
+    private final String descricao;
+    TipoLancamento(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
 }
